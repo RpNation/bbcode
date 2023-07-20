a71cc3b4f64c144416ac0adeb37adf0448c7fd9e
Steven Chang <42795314+Alteras1@users.noreply.github.com>
2023-07-20 14:21:07 -0700

    Copy Discourse plugin skeleton

---
+++ plugin.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# name: BBCode
+# about: TODO
+# version: 0.0.1
+# authors: RpNation
+# url: https://github.com/RpNation/BBCode
+# required_version: 2.7.0
+
+enabled_site_setting :bbcode_enabled
+
+after_initialize do
+  # Code which should run after Rails has finished booting
+end
