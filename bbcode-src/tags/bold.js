d8e91390198b540007294dce5af2e3374867f337
MShultz <mary.shultz.2018@gmail.com>
2023-08-21 00:25:35 -0500

    Removes unneeed bold changes

--- bbcode-src/tags/bold.js
+++
@@ -1,8 +0,0 @@
-export const bold = (node) => {
-  // console.log(node);
-  return {
-    tag: "span",
-    attr: { style: "font-weight: bold", 'data-rpn-bbcode': '' },
-    content: node.content,
-  };
-};
