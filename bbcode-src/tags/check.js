65cc77af318bd9e911811ef5bf347090b3611675
hartleyk1 <wolfslegend@gmail.com>
2024-01-06 10:10:11 -0500

    Adjusted SASS to correctly display icon for the right check type

--- bbcode-src/tags/check.js
+++ bbcode-src/tags/check.js
@@ -2,5 +2,5 @@ import { preprocessAttr, toNode } from "../utils/common";
 
 export const check = (node) => {
     const attrs = preprocessAttr(node.attrs)._default || "dot";
-    return toNode("div",  { class: `bb-check`,"data-type": attrs },  node.content );
+    return toNode("div",  { class: `bb-check`, "data-type": attrs },  node.content );
 };
