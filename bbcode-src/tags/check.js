a6a4ea46bf61371c69673bd1b0671911fbfe72d0
tolliverk <tolliverkl@etsu.edu>
2024-01-05 23:47:45 -0500

    Applying default

--- bbcode-src/tags/check.js
+++ bbcode-src/tags/check.js
@@ -1,17 +1,6 @@
 import { preprocessAttr, toNode } from "../utils/common";
 
-/**
- * @file Adds [check] to bbcode
- * @example [check]content[/check]
- */
 export const check = (node) => {
-    const type = (preprocessAttr(node.attrs)._default || "dot").toLowerCase();
-  return toNode(
-        "div", 
-        { 
-            class: `bb-check`,
-            "data-type": type
-        }, 
-        node.content
-    );
+    const attrs = preprocessAttr(node.attrs)._default || "dot";
+    return toNode("div",  { class: `bb-check`,"data-type": attrs },  node.content );
 };
