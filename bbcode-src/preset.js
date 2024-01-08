baa9e4d9d4b136e661620bb1097933fd77f8bbcd
MShultz <mary.shultz.2018@gmail.com>
2024-01-07 21:00:57 -0600

    Renames alignment tag export to match convention

--- bbcode-src/preset.js
+++ bbcode-src/preset.js
@@ -2,7 +2,7 @@ import { createPreset } from "@bbob/preset";
 import { font } from "./tags/font";
 import { nobr } from "./tags/nobr";
 import { highlight } from "./tags/highlight";
-import { alignmenttags } from "./tags/alignment";
+import { alignment } from "./tags/alignment";
 import { inlinespoiler, spoiler } from "./tags/spoiler";
 import { color } from "./tags/color";
 import { size } from "./tags/size";
@@ -16,9 +16,9 @@ import { justify } from "./tags/justify";
 import { check } from "./tags/check";
 
 const tags = {
+  ...alignment,
   font,
   nobr,
-  ...alignmenttags,
   highlight,
   spoiler,
   inlinespoiler,
