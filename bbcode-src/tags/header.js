093ea92a3b4ebaadde68310f7ac0d7a9ea45f314
J. Pryce <71295464+kyuukestu@users.noreply.github.com>
2024-01-20 02:28:10 -0500

    Update header.js

--- bbcode-src/tags/header.js
+++ bbcode-src/tags/header.js
@@ -7,35 +7,35 @@ import { toNode } from "../utils/common";
  */
 
 const h = (node) => {
-  return toNode("h1", node.content);
+  return toNode("h1", {}, node.content);
 };
 
 const h1 = (node) => {
-  return toNode("h1", node.content);
+  return toNode("h1", {}, node.content);
 };
 
 const h2 = (node) => {
-  return toNode("h2", node.content);
+  return toNode("h2", {}, node.content);
 };
 
 const sh = (node) => {
-  return toNode("h2", node.content);
+  return toNode("h2", {}, node.content);
 };
 
 const h3 = (node) => {
-  return toNode("h3", node.content);
+  return toNode("h3", {}, node.content);
 };
 
 const h4 = (node) => {
-  return toNode("h4", node.content);
+  return toNode("h4", {}, node.content);
 };
 
 const h5 = (node) => {
-  return toNode("h5", node.content);
+  return toNode("h5", {}, node.content);
 };
 
 const h6 = (node) => {
-  return toNode("h6", node.content);
+  return toNode("h6", {}, node.content);
 };
 
 export { h, sh, h1, h2, h3, h4, h5, h6 };
