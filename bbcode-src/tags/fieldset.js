a5b858965bedeb322ef3d2943edbe87f75828e20
hartleyk1 <wolfslegend@gmail.com>
2024-02-29 11:07:57 -0500

    Attempting to resolve prettier pipeline failures

--- bbcode-src/tags/fieldset.js
+++ bbcode-src/tags/fieldset.js
@@ -1,11 +1,11 @@
 import { preprocessAttr, toNode } from "../utils/common";
     
 /**
- * @file Adds [fieldset] tp bbcode
+ * @file Adds [fieldset] to bbcode
  * @example [fieldset=title]content[/fieldset]
  */
 export const fieldset = (node) => {
-    const title = preprocessAttr(node.attrs)._default || '';
+    const title = preprocessAttr(node.attrs)._default || "";
     return {
         tag: "fieldset",
         attrs: {
@@ -15,17 +15,17 @@ export const fieldset = (node) => {
             {
                 tag: "legend",
                 attrs: {
-                class: "bb-fieldset-legend"
+                    class: "bb-fieldset-legend"
                 },
                 content: title
             },
             {
                 tag: "span",
                 attrs: {
-                class: "bb-fieldset"
+                    class: "bb-fieldset"
                 },
                 content: node.content
             }
-        ],
+        ]
     };
-}
+};
