1959b4fc4fcb273dd92d46fb275f83188ad0d51a
J. Pryce <71295464+kyuukestu@users.noreply.github.com>
2024-03-01 01:24:47 -0500

    Update subscript.js
    Node content correction.

--- bbcode-src/tags/subscript.js
+++ bbcode-src/tags/subscript.js
@@ -1,12 +1,12 @@
 import { toNode } from "../utils/common";
 
 /**
- * @file Adds subscript to bbcode
+ * @file Adds subscript to BBCode
  * @example [sub]content[/sub]
  */
 
-const sub = () => {
-  return toNode("sub", {}, null);
+const sub = (node) => {
+  return toNode("sub", {}, node.content);
 };
 
 export { sub };
