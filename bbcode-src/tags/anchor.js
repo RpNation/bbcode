1f1c20173ea87913bcfa4313e4080b00643dbf96
hartleyk1 <wolfslegend@gmail.com>
2024-03-09 13:23:25 -0500

    Update anchor id tag

--- bbcode-src/tags/anchor.js
+++ bbcode-src/tags/anchor.js
@@ -4,6 +4,7 @@ import { preprocessAttr, toNode } from "../utils/common";
  * @example [a=your_anchor_name]An anchor[/a] [goto=your_anchor_name]Jump to an anchor[/goto]
  */
 export const anchor = {
-    a: (node) => toNode("a", { id: `user-anchor-${preprocessAttr(node.attrs)._default}` }, node.content),
+    // name is not valid in HTML5; however, it correctly displays back while id does not
+    a: (node) => toNode("a", { id: `user-anchor-${preprocessAttr(node.attrs)._default}`, name: `user-anchor-${preprocessAttr(node.attrs)._default}` }, node.content),
     goto: (node) => toNode("a", { href: `#user-anchor-${preprocessAttr(node.attrs)._default}` }, node.content)
 };
