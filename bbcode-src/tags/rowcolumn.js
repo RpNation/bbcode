5b65e3f7e9a23eed789ffd975089e53a71e375ab
hartleyk1 <wolfslegend@gmail.com>
2024-04-13 19:49:12 -0400

    Adjustments

--- bbcode-src/tags/rowcolumn.js
+++ bbcode-src/tags/rowcolumn.js
@@ -8,7 +8,9 @@ export const rowcolumn = {
   row: (node) => toNode("div", { class: "bb-row" }, node.content),
   column: (node) => {
     const columnAttrs = preprocessAttr(node.attrs)._default || "1";
-    const columnStyle = columnAttrs.startsWith("span") ? `column-width-${columnAttrs}` : `column-width-span${columnAttrs}`;
-      return toNode("div", { class: `bb-column`, "data-span": columnStyle }, node.content);
-  }
+    const columnStyle = columnAttrs.startsWith("span")
+      ? `column-width-${columnAttrs}`
+      : `column-width-span${columnAttrs}`;
+    return toNode("div", { class: `bb-column`, "data-span": columnStyle }, node.content);
+  },
 };
