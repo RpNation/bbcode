4b67933647c2052919e5eeefd45db43002e361e3
hartleyk1 <wolfslegend@gmail.com>
2024-04-13 19:08:26 -0400

    Workaround for multi class

--- bbcode-src/tags/rowcolumn.js
+++ bbcode-src/tags/rowcolumn.js
@@ -7,10 +7,8 @@ import { preprocessAttr, toNode } from "../utils/common";
 export const rowcolumn = {
   row: (node) => toNode("div", { class: "bb-row" }, node.content),
   column: (node) => {
-    const columnAttrs = preprocessAttr(node.attrs)._default || "";
-    const columnStyle = columnAttrs.startsWith("span")
-      ? `bb-column column-width-${columnAttrs}`
-      : `bb-column column-width-span${columnAttrs}`;
-    return toNode("div", { class: `bb-column ${columnStyle}` }, node.content);
-  },
+    const columnAttrs = preprocessAttr(node.attrs)._default || "1";
+    const columnStyle = columnAttrs.startsWith("span") ? `column-width-${columnAttrs}` : `column-width-span${columnAttrs}`;
+      return toNode("div", { class: `bb-column`, "data-span": columnStyle }, node.content);
+  }
 };
