c115d08e67be97d652495f2fdc727f2f672010d8
hartleyk1 <wolfslegend@gmail.com>
2024-04-17 17:17:41 -0400

    Set default to span 8

--- bbcode-src/tags/rowcolumn.js
+++ bbcode-src/tags/rowcolumn.js
@@ -7,7 +7,7 @@ import { preprocessAttr, toNode } from "../utils/common";
 export const rowcolumn = {
   row: (node) => toNode("div", { class: "bb-row" }, node.content),
   column: (node) => {
-    const columnAttrs = preprocessAttr(node.attrs)._default || "1";
+    const columnAttrs = preprocessAttr(node.attrs)._default || "8";
     const columnStyle = columnAttrs.startsWith("span")
       ? `column-width-${columnAttrs}`
       : `column-width-span${columnAttrs}`;
