193c5b47087c1aca283664670db068933864691b
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2024-05-22 14:15:26 -0700

    clean up editor config

--- bbcode-src/tags/progress.js
+++ bbcode-src/tags/progress.js
@@ -1,38 +1,37 @@
-
-import { preprocessAttr, toNode } from "../utils/common";
+import { preprocessAttr } from "../utils/common";
 
 /**
  * @file Adds [progress] to bbcode
  * @exmaple [progress=percentageInt]content[/progress]
  */
 export const progress = (node) => {
-    const percentageInt = preprocessAttr(node.attrs)._default;
-    return {
+  const percentageInt = preprocessAttr(node.attrs)._default;
+  return {
+    tag: "div",
+    attrs: {
+      class: "bb-progress",
+    },
+    content: [
+      {
         tag: "div",
         attrs: {
-          class: "bb-progress",
+          class: "bb-progress-text",
         },
-        content: [
-          {
-            tag: "div",
-            attrs: {
-              class: "bb-progress-text"
-            },
-            content: node.content
-          },
-          {
-            tag: "div",
-            attrs: {
-              class: "bb-progress-bar",
-              style: `width: calc(${percentageInt}% - 6px)`
-            }
-          },
-          {
-            tag: "div",
-            attrs: {
-              class: "bb-progress-bar-other"
-            }
-          }
-        ],
-      };
-}
+        content: node.content,
+      },
+      {
+        tag: "div",
+        attrs: {
+          class: "bb-progress-bar",
+          style: `width: calc(${percentageInt}% - 6px)`,
+        },
+      },
+      {
+        tag: "div",
+        attrs: {
+          class: "bb-progress-bar-other",
+        },
+      },
+    ],
+  };
+};
