03abaccb5439b5df67f83a5224de66beb03ec896
Steven Chang <42795314+Alteras1@users.noreply.github.com>
2024-07-17 14:38:30 -0700

    chore: Update npm dependencies and add lint script (#126)
    * chore: Update npm dependencies and add lint script
    
    * Update bbob version
    
    * Fix bbob rollup node resolve injecting env variables check

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 import terser from "@rollup/plugin-terser";
 
 export default [
@@ -14,7 +15,16 @@ export default [
       },
     },
     external: ["jquery"],
-    plugins: [nodeResolve(), terser({ format: { preamble: "/* Source code in bbcode-src */" } })],
+    plugins: [
+      nodeResolve(),
+      replace({
+        preventAssignment: true,
+        values: {
+          "process.env.NODE_ENV": JSON.stringify("production"),
+        },
+      }),
+      terser({ format: { preamble: "/* Source code in bbcode-src */" } }),
+    ],
     watch: {
       include: "bbcode-src/**",
       exclude: "bbcode-src/bbscript/**",
