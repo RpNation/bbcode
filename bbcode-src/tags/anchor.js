80c46e5277d0194b44d6020e3492f906c816228b
Steven Chang <42795314+Alteras1@users.noreply.github.com>
2024-07-25 21:15:46 -0700

    build parser for goto (#130)

--- bbcode-src/tags/anchor.js
+++ bbcode-src/tags/anchor.js
@@ -1,16 +1,20 @@
 import { preprocessAttr, toNode } from "../utils/common";
 /**
- * @file Adds [id] and [goto] to bbcode
+ * @file Adds [a] and [goto] to bbcode
  * @example [a=your_anchor_name]An anchor[/a] [goto=your_anchor_name]Jump to an anchor[/goto]
  */
 export const anchor = {
-    // name is not valid in HTML5; however, it correctly displays back while id does not
-    a: (node) => {
-        const attrs = preprocessAttr(node.attrs)._default || "";
-        return toNode("a", { id: `user-anchor-${attrs.trim()}`, name: `user-anchor-${attrs.trim()}` }, node.content);
-      },
-      goto: (node) => {
-        const attrs = preprocessAttr(node.attrs)._default || "";
-        return toNode("a", { href: `#user-anchor-${attrs.trim()}` }, node.content);
-      }
+  // name is not valid in HTML5; however, it correctly displays back while id does not
+  a: (node) => {
+    const attrs = preprocessAttr(node.attrs)._default || "";
+    return toNode(
+      "a",
+      { id: `user-anchor-${attrs.trim()}`, name: `user-anchor-${attrs.trim()}` },
+      node.content,
+    );
+  },
+  goto: (node) => {
+    const attrs = preprocessAttr(node.attrs)._default || "";
+    return toNode("a", { href: `#user-anchor-${attrs.trim()}` }, node.content);
+  },
 };
